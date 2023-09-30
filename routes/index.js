68f90051a7aee8605d8b58cb2f01cc83f6019aa2
rybak2Z <rybak2Z.000@gmail.com>
2023-09-30 14:30:45 +0200

    Add example messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Do you love me?',
+    user: 'She',
+    added: new Date(),
+  },
+  {
+    text: 'Only partly',
+    user: 'Drake',
+    added: new Date(),
+  },
+  {
+    text: "I only love my bed and my momma, I'm sorry",
+    user: 'Drake',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages });
 });
 
 module.exports = router;
